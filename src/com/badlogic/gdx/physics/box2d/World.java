6ff2bf3f67ec93f0541fb62ee9d5cd00cceb1f03
nmtv3386 <thomas.millet@orange.com>
2016-01-23 00:49:34 +0100

    Full merge

--- src/com/badlogic/gdx/physics/box2d/World.java
+++ src/com/badlogic/gdx/physics/box2d/World.java
@@ -79,6 +79,7 @@ static jmethodID reportRayParticleID = 0;
 static jmethodID rayShouldQueryParticleSystemID = 0;
 static jmethodID particleBodyContactListenerBeginContactID = 0;
 
+
 class CustomRayCastCallback: public b2RayCastCallback
 {
 private:
@@ -97,7 +98,7 @@ public:
 		return env->CallFloatMethod(obj, reportRayFixtureID, (jlong)fixture, (jfloat)point.x, (jfloat)point.y,
 																(jfloat)normal.x, (jfloat)normal.y, (jfloat)fraction );
 	}
-	
+
 	virtual float32 ReportParticle(const b2ParticleSystem* particleSystem, int32 index, const b2Vec2& point,
 								   												const b2Vec2& normal, float32 fraction)
 	{
@@ -159,7 +160,7 @@ public:
 			if( endContactID != 0 )
 				env->CallVoidMethod(obj, endContactID, (jlong)contact);
 		}
-		
+
 		/// Called when a particle and a fixture begin to touch
 		virtual void BeginContact(b2ParticleSystem* particleSystem, b2ParticleBodyContact* particleBodyContact)
 		{
@@ -192,14 +193,14 @@ public:
 		virtual void PreSolve(b2Contact* contact, const b2Manifold* oldManifold)
 		{
 			if( preSolveID != 0 )
-				env->CallVoidMethod(obj, preSolveID, (jlong) contact, (jlong) oldManifold);
+				env->CallVoidMethod(obj, preSolveID, (jlong)contact, (jlong)oldManifold);
 		}
 	
 		/// This lets you inspect a contact after the solver is finished.
 		virtual void PostSolve(b2Contact* contact, const b2ContactImpulse* impulse)
 		{
 			if( postSolveID != 0 )
-				env->CallVoidMethod(obj, postSolveID, (jlong) contact, (jlong) impulse);
+				env->CallVoidMethod(obj, postSolveID, (jlong)contact, (jlong)impulse);
 		}
 };
 
@@ -220,7 +221,7 @@ public:
 	{
 		return env->CallBooleanMethod(obj, reportFixtureID, (jlong)fixture );
 	}
-	
+
 	virtual bool ReportParticle( const b2ParticleSystem* particleSystem, 
 																			int32 index ) 
 	{
@@ -231,7 +232,7 @@ public:
 	{
 		return env->CallBooleanMethod(obj, shouldQueryParticleSystemID, (jlong) particleSystem);
 	}
-};
+}; 
 
 class CustomParticleBodyContactListener: public b2ParticleBodyContactListener
 {
@@ -289,17 +290,17 @@ b2ContactFilter defaultFilter;
 	};
 
 	/** the address of the world instance **/
-	private final long addr;
+	protected final long addr;
 
 	/** all known bodies **/
-	public final LongMap<Body> bodies = new LongMap<Body>(100);
+	protected final LongMap<Body> bodies = new LongMap<Body>(100);
 
 	/** all known fixtures **/
-	public final LongMap<Fixture> fixtures = new LongMap<Fixture>(100);
-	
+	protected final LongMap<Fixture> fixtures = new LongMap<Fixture>(100);
+
 	/** all known particlesystems **/
 	public final LongMap<ParticleSystem> particleSystems = new LongMap<ParticleSystem>(100);
-
+	
 	/** all known joints **/
 	protected final LongMap<Joint> joints = new LongMap<Joint>(100);
 
@@ -308,7 +309,7 @@ b2ContactFilter defaultFilter;
 
 	/** Contact listener **/
 	protected ContactListener contactListener = null;
-	
+
 	/** ParticleBody contact listener **/
 	protected ParticleBodyContactListener particleBodyContactListener = null;
 
@@ -425,8 +426,11 @@ b2ContactFilter defaultFilter;
 		this.bodies.remove(body.addr);
 		Array<Fixture> fixtureList = body.getFixtureList();
 		while(fixtureList.size > 0) {
-			this.fixtures.remove(fixtureList.removeIndex(0).addr).setUserData(null);
-		}
+			Fixture fixtureToDelete = fixtureList.removeIndex(0);
+ 			this.fixtures.remove(fixtureToDelete.addr).setUserData(null);
+ 			freeFixtures.free(fixtureToDelete);
+ 		}
+		
 		freeBodies.free(body);
 	}
 
@@ -438,6 +442,46 @@ b2ContactFilter defaultFilter;
 		world->SetContactFilter(&contactFilter);
 		world->SetContactListener(&contactListener);
 		world->DestroyBody(body);
+		world->SetContactFilter(&defaultFilter);
+		world->SetContactListener(0);
+	*/
+	
+	/** Internal method for fixture destruction with notifying custom
+	 * contact listener
+	 * @param body
+	 * @param fixture */
+	void destroyFixture(Body body, Fixture fixture) {
+		jniDestroyFixture(addr, body.addr, fixture.addr);
+	}
+	
+	private native void jniDestroyFixture(long addr, long bodyAddr, long fixtureAddr); /*
+		b2World* world = (b2World*)(addr);
+		b2Body* body = (b2Body*)(bodyAddr);
+		b2Fixture* fixture = (b2Fixture*)(fixtureAddr);
+		CustomContactFilter contactFilter(env, object);
+		CustomContactListener contactListener(env, object);
+		world->SetContactFilter(&contactFilter);
+		world->SetContactListener(&contactListener);
+		body->DestroyFixture(fixture);
+		world->SetContactFilter(&defaultFilter);
+		world->SetContactListener(0);
+	*/
+	
+	/** Internal method for body deactivation with notifying custom
+	 * contact listener
+	 * @param body */
+	void deactivateBody(Body body) {
+		jniDeactivateBody(addr, body.addr);
+	}
+	
+	private native void jniDeactivateBody(long addr, long bodyAddr); /*
+		b2World* world = (b2World*)(addr);
+		b2Body* body = (b2Body*)(bodyAddr);	
+		CustomContactFilter contactFilter(env, object);
+		CustomContactListener contactListener(env, object);
+		world->SetContactFilter(&contactFilter);
+		world->SetContactListener(&contactListener);
+		body->SetActive(false);
 		world->SetContactFilter(&defaultFilter);
 		world->SetContactListener(0);
 	*/
@@ -521,7 +565,7 @@ b2ContactFilter defaultFilter;
 		if (def.type == JointType.WeldJoint) {
 			WeldJointDef d = (WeldJointDef)def;
 			return jniCreateWeldJoint(addr, d.bodyA.addr, d.bodyB.addr, d.collideConnected, d.localAnchorA.x, d.localAnchorA.y,
-				d.localAnchorB.x, d.localAnchorB.y, d.referenceAngle);
+				d.localAnchorB.x, d.localAnchorB.y, d.referenceAngle, d.frequencyHz, d.dampingRatio);
 		}
 		if (def.type == JointType.WheelJoint) {
 			WheelJointDef d = (WheelJointDef)def;
@@ -701,7 +745,7 @@ b2ContactFilter defaultFilter;
 	*/
 
 	private native long jniCreateWeldJoint (long addr, long bodyA, long bodyB, boolean collideConnected, float localAnchorAX,
-		float localAnchorAY, float localAnchorBX, float localAnchorBY, float referenceAngle); /*
+		float localAnchorAY, float localAnchorBX, float localAnchorBY, float referenceAngle, float frequencyHz, float dampingRatio); /*
 		b2World* world = (b2World*)addr;
 		b2WeldJointDef def;
 		def.bodyA = (b2Body*)bodyA;
@@ -710,6 +754,8 @@ b2ContactFilter defaultFilter;
 		def.localAnchorA = b2Vec2(localAnchorAX, localAnchorAY);
 		def.localAnchorB = b2Vec2(localAnchorBX, localAnchorBY);
 		def.referenceAngle = referenceAngle;
+		def.frequencyHz = frequencyHz;
+		def.dampingRatio = dampingRatio;
 	
 		return (jlong)world->CreateJoint(&def);
 	*/
@@ -811,6 +857,11 @@ b2ContactFilter defaultFilter;
 		b2World* world = (b2World*)addr;
 		return world->GetBodyCount();
 	*/
+	
+	/** Get the number of fixtures. */
+	public int getFixtureCount () {
+		return fixtures.size;
+	}
 
 	/** Get the number of joints. */
 	public int getJointCount () {
@@ -932,7 +983,7 @@ b2ContactFilter defaultFilter;
 	private final Array<Contact> contacts = new Array<Contact>();
 	private final Array<Contact> freeContacts = new Array<Contact>();
 
-	/** Returns the list of {@link Contact} instances produced by the last call to {@link #step(float, int, int, int)}. Note that the
+	/** Returns the list of {@link Contact} instances produced by the last call to {@link #step(float, int, int)}. Note that the
 	 * returned list will have O(1) access times when using indexing. contacts are created and destroyed in the middle of a time
 	 * step. Use {@link ContactListener} to avoid missing contacts
 	 * @return the contact list */
@@ -967,6 +1018,15 @@ b2ContactFilter defaultFilter;
 		bodies.ensureCapacity(this.bodies.size);
 		for (Iterator<Body> iter = this.bodies.values(); iter.hasNext();) {
 			bodies.add(iter.next());
+		}		
+	}
+
+	/** @param fixtures an Array in which to place all fixtures currently in the simulation */
+	public void getFixtures (Array<Fixture> fixtures) {
+		fixtures.clear();
+		fixtures.ensureCapacity(this.fixtures.size);
+		for (Iterator<Fixture> iter = this.fixtures.values(); iter.hasNext();) {
+			fixtures.add(iter.next());
 		}		
 	}
 
@@ -1035,7 +1095,7 @@ b2ContactFilter defaultFilter;
 		contact.addr = contactAddr;
 		if (contactListener != null) contactListener.endContact(contact);
 	}
-	
+
 	private void beginParticleBodyContact (long particleSystemAddr, long contactAddr) {
 		particleBodyContact.addr = contactAddr;
 		particleBodyContact.particleSystem = this.particleSystems.get(particleSystemAddr);
@@ -1078,7 +1138,7 @@ b2ContactFilter defaultFilter;
 		else
 			return false;
 	}
-	
+
 	private boolean reportParticle (long particleSystemAddr, int index) {
 		if (queryCallback != null)
 			return queryCallback.reportParticle(this.particleSystems.get(particleSystemAddr), index);
@@ -1092,6 +1152,17 @@ b2ContactFilter defaultFilter;
 		else
 			return false;
 	}
+
+	/** Sets the box2d velocity threshold globally, for all World instances.
+	 * @param threshold the threshold, default 1.0f */
+	public static native void setVelocityThreshold (float threshold); /*
+		b2_velocityThreshold = threshold;
+	*/
+
+	/** @return the global box2d velocity threshold. */
+	public static native float getVelocityThreshold (); /*
+		return b2_velocityThreshold;
+	*/
 	
 	/** Ray-cast the world for all fixtures in the path of the ray. The ray-cast ignores shapes that contain the starting point.
 	 * @param callback a user implemented callback class.
@@ -1124,7 +1195,7 @@ b2ContactFilter defaultFilter;
 			return 0.0f;
 		}
 	}
-	
+
 	private float reportRayParticle(long particleSystemAddr, int index, float pointX, float pointY, 
 		float normalX, float normalY, float fraction) {
 		if(rayCastCallback != null) {
--- src/com/badlogic/gdx/physics/box2d/utils/Box2DBuild.java
+++ src/com/badlogic/gdx/physics/box2d/utils/Box2DBuild.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.jnigen.BuildTarget.TargetOs;
 
 public class Box2DBuild {
 	public static void main(String[] args) throws Exception {
-		BuildTarget win32 = BuildTarget.newDefaultTarget(TargetOs.Windows, false); win32.compilerPrefix = "mingw32-";
+		BuildTarget win32 = BuildTarget.newDefaultTarget(TargetOs.Windows, false);
 		BuildTarget win64 = BuildTarget.newDefaultTarget(TargetOs.Windows, true);
 		BuildTarget lin32 = BuildTarget.newDefaultTarget(TargetOs.Linux, false);
 		BuildTarget lin64 = BuildTarget.newDefaultTarget(TargetOs.Linux, true);
@@ -19,6 +19,6 @@ public class Box2DBuild {
 		BuildTarget mac64 = BuildTarget.newDefaultTarget(TargetOs.MacOsX, true);
 		BuildTarget ios = BuildTarget.newDefaultTarget(TargetOs.IOS, false);
 		new NativeCodeGenerator().generate("src", "bin" + File.pathSeparator + "../../../gdx/bin", "jni");
-		new AntScriptGenerator().generate(new BuildConfig("gdx-liquidfun"), win32, win64, lin32, lin64, mac32, mac64, android, ios);		
+		new AntScriptGenerator().generate(new BuildConfig("gdx-box2d"), win32, win64, lin32, lin64, mac32, mac64, android, ios);		
 	}
 }
