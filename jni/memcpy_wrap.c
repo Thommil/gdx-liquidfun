0ab3d77b770cf164344cd1c89e37f9a84e2cb49d
nmtv3386 <thomas.millet@orange.com>
2016-01-23 03:07:39 +0100

    First build OK

--- jni/memcpy_wrap.c
+++ jni/memcpy_wrap.c
@@ -1,18 +1,39 @@
+// #ifndef __ANDROID__
+// #ifdef __linux__
+// #ifdef __x86_64__
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+
+// void *__wrap_memcpy(void * destination, const void * source, size_t num)
+// {
+//         return memcpy(destination, source, num);
+// }
+// #else
+
+// #endif
+// #endif
+// #endif
+
 #ifndef __ANDROID__
-#ifdef __linux__
-#ifdef __x86_64__
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
-
-void *__wrap_memcpy(void * destination, const void * source, size_t num)
+void* memcpy (void *destination, const void *source, size_t num)
 {
-        return memcpy(destination, source, num);
+    char *D = (char*)destination;
+    char *S = (char*)source;
+    for(int i = 0; i < num; i++) 
+    {
+            *D = *S;
+            D++;
+            S++;
+    }
+    return D;
 }
 
-#endif
-#endif
 #endif
